866cfcf952002e7d7c44b11b8aecbb9c3aab446b
liushuai <1847144487@qq.com>
2020-07-31 19:56:34 +0800

    创建User

---
+++ user/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,46 @@
+package com.itheima.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String password;
+    private double money;
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", money=" + money +
+                '}';
+    }
+
+    //setter&getter
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+}
